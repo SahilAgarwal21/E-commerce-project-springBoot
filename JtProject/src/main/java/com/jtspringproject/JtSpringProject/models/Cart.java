686be434197071986523cc28bc4e14b9bdd61782
Sahil Agarwal <sahil11875@gmail.com>
2025-07-05 18:20:09 +0530

    updated

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/models/Cart.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/models/Cart.java
@@ -61,7 +61,7 @@ public class Cart {
 //    }
 
    public void setProducts(List<Product> products) {
-       this.products = "test4";
+       this.products = "test5";
    }
 
 //    public void addProduct(Product product) {
